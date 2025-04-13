d4279d5a4e840107aafec84ef9a3e6cd14ff8e76
bcoalova <coalovaboris@gmail.com>
2025-04-13 00:30:16 -0300

    to

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from 'lovable-tagger'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    // base: '/routine-clock/',
+    base: '/routine-clock',
     server: {
         host: '::',
         port: 8080,
